ae1bc1516063dc4b114d36a6ac04274559f56a1a
bohdanrysakov <bogdanrysokov@gmail.com>
2023-08-31 15:50:29 +0300

    initial commit

---
+++ src/main/kotlin/rys/ajaxpetproject/AjaxPetProjectApplication.kt
@@ -0,0 +1,11 @@
+package rys.ajaxpetproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AjaxPetProjectApplication
+
+fun main(args: Array<String>) {
+    runApplication<AjaxPetProjectApplication>(*args)
+}
