18deff3d598cdf40c50da2fa99ccf1781704cc96
bohdanrysakov <bogdanrysokov@gmail.com>
2023-09-03 19:18:33 +0300

    check-1

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,14 +28,14 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-data-mongodb
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb:3.0.10")
-    // https://mvnrepository.com/artifact/log4j/log4j
     implementation("log4j:log4j:1.2.17")
-    // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-logging
     implementation("org.springframework.boot:spring-boot-starter-logging:3.1.0")
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
