5d62fa4fa95908eb74498234ff3c7e33cae39c28
bohdanrysakov <bogdanrysokov@gmail.com>
2023-09-11 16:00:59 +0300

    Initial Project

--- src/main/kotlin/rys/ajaxpetproject/controller/MessageController.kt
+++ src/main/kotlin/rys/ajaxpetproject/controller/MessageController.kt
@@ -4,7 +4,14 @@ import jakarta.validation.Valid
 import org.bson.types.ObjectId
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.GetMapping
 import rys.ajaxpetproject.model.MongoMessage
 import rys.ajaxpetproject.service.MessageService
 
@@ -18,11 +25,11 @@ class MessageController(val messageService: MessageService) {
 
     @GetMapping("/{id}")
     fun getMessageById(@PathVariable id: ObjectId): ResponseEntity<MongoMessage> =
-         ResponseEntity(messageService.getMessageById(id), HttpStatus.OK)
+         ResponseEntity(messageService.findMessageById(id), HttpStatus.OK)
 
     @GetMapping("/chat/{chatId}")
     fun getAllMessagesByChatId(@PathVariable chatId: ObjectId): ResponseEntity<List<MongoMessage>> =
-        ResponseEntity(messageService.getAllMessagesByChatId(chatId), HttpStatus.OK)
+        ResponseEntity(messageService.findAllMessagesByChatId(chatId), HttpStatus.OK)
 
     @PutMapping("/{id}")
     fun updateMessage(@PathVariable id: ObjectId, @Valid @RequestBody updatedMongoMessage: MongoMessage):
--- src/test/kotlin/rys/ajaxpetproject/AjaxPetProjectApplicationTests.kt
+++ src/test/kotlin/rys/ajaxpetproject/AjaxPetProjectApplicationTests.kt
@@ -1,13 +1,6 @@
 package rys.ajaxpetproject
 
-import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class AjaxPetProjectApplicationTests {
-
-    @Test
-    fun contextLoads() {
-    }
-
-}
+class AjaxPetProjectApplicationTests
