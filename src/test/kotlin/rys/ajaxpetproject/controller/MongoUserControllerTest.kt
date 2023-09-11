565212cc44e72f9f7c8b2f12032f5a240f135d17
bohdanrysakov <bogdanrysokov@gmail.com>
2023-09-11 11:06:52 +0300

    fix #3 test should be ok

--- src/test/kotlin/rys/ajaxpetproject/controller/MongoUserControllerTest.kt
+++ src/test/kotlin/rys/ajaxpetproject/controller/MongoUserControllerTest.kt
@@ -53,7 +53,8 @@ class MongoUserControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
         )
             .andExpect(status().isOk)
-            .andExpect(jsonPath("$.id").value(testMongoUser.id.toString()))
+            .andExpect(jsonPath("$.id.timestamp").value(1694093823))
+            .andExpect(jsonPath("$.id.date").value("2023-09-07T13:37:03.000+00:00"))
             .andExpect(jsonPath("$.userName").value(testMongoUser.userName))
             .andExpect(jsonPath("$.password").value(testMongoUser.password))
             .andReturn()
@@ -62,11 +63,13 @@ class MongoUserControllerTest {
     @Test
     fun getUserFailDueToBadRequestTest() {
         val testMongoUser = MongoUser(
-            userName = "testUser", password =
-            BCryptPasswordEncoder().encode("testPassword")
+            id = ObjectId(Date(1694093823L * 1000)),
+            userName = "testUser",
+            password = BCryptPasswordEncoder().encode("testPassword")
         )
 
-        `when`(userService.findUserById(testMongoUser.id as ObjectId)).thenReturn(testMongoUser)
+        `when`(userService.findUserById(testMongoUser.id as ObjectId))
+            .thenReturn(testMongoUser)
 
         mockMvc.perform(
             get("/users/SomeString")
@@ -78,8 +81,9 @@ class MongoUserControllerTest {
     @Test
     fun createUserFailDueToValidationTest() {
         val testMongoUser = MongoUser(
+            id = ObjectId(Date(1694093823L * 1000)),
             userName = "12",
-            password = "www"
+            password = "12"
         )
         val objectMapper = ObjectMapper()
 
@@ -92,111 +96,16 @@ class MongoUserControllerTest {
                 jsonPath(
                     "message", anyOf(
                         equalTo(
-                            "Password must be between 8 and 100 characters, " +
+                            "Password must be between 3 and 100 characters, " +
                                     "Username must be between 3 and 20 characters"
                         ), equalTo(
-                            "Username must be between 3 and 20 characters," +
-                                    " Password must be between 8 and 100 characters"
+                            "Username must be between 3 and 20 characters, " +
+                                    "Password must be between 3 and 100 characters"
                         )
                     )
                 ),
                 jsonPath("status").value(400)
             )
             .andExpect(status().isBadRequest)
-
     }
 }
-/*
-package rys.ajaxpetproject.controller
-
-import com.fasterxml.jackson.databind.ObjectMapper
-import lombok.AllArgsConstructor
-import lombok.NoArgsConstructor
-import lombok.RequiredArgsConstructor
-import org.bson.types.ObjectId
-import org.hamcrest.Matchers.equalTo
-import com.nhaarman.mockitokotlin2.whenever
-import org.hamcrest.Matchers.anyOf
-import org.junit.jupiter.api.Test
-import org.mockito.Mockito.`when`
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-import org.springframework.boot.test.mock.mockito.MockBean
-import org.springframework.http.MediaType
-import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
-import rys.ajaxpetproject.model.MongoUser
-import rys.ajaxpetproject.service.UserService
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-
-@WebMvcTest(UserController::class)
-@RequiredArgsConstructor
-@AllArgsConstructor
-@NoArgsConstructor
-class MongoUserControllerTest {
-    @MockBean
-    lateinit var userService: UserService
-
-    @Autowired
-    lateinit var mockMvc: MockMvc
-
-    @Test
-    fun getUserSuccessfullyTest() {
-        val testMongoUser = MongoUser(
-            userName = "testUser",
-            password = BCryptPasswordEncoder().encode("testPassword")
-        )
-        whenever(userService.findUserById(testMongoUser.id as ObjectId)).thenReturn(testMongoUser)
-
-        mockMvc.perform(
-            get("/users/${testMongoUser.id}").contentType(MediaType.APPLICATION_JSON)
-        ).andExpect(status().isOk).andExpect(jsonPath("$.id").value(testMongoUser.id.toString()))
-            .andExpect(jsonPath("$.userName").value(testMongoUser.userName))
-            .andExpect(jsonPath("$.password").value(testMongoUser.password)).andReturn()
-    }
-
-    @Test
-    fun getUserFailDueToBadRequestTest() {
-        val testMongoUser = MongoUser(
-            userName = "testUser",
-            password = BCryptPasswordEncoder().encode("testPassword")
-        )
-
-        `when`(userService.findUserById(testMongoUser.id as ObjectId)).thenReturn(testMongoUser)
-
-        mockMvc.perform(
-            get("/users/SomeString").contentType(MediaType.APPLICATION_JSON)
-        ).andExpect(status().isBadRequest)
-    }
-
-    @Test
-    fun createUserFailDueToValidationTest() {
-        val testMongoUser = MongoUser(
-            userName = "12",
-            password = "www"
-        )
-        val objectMapper = ObjectMapper()
-
-        mockMvc.perform(
-            post("/users/").contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(testMongoUser))
-        ).andExpectAll(
-                jsonPath(
-                    "message", anyOf(
-                        equalTo(
-                            "Password must be between 8 and 100 characters, " +
-                                    "Username must be between 3 and 20 characters"
-                        ), equalTo(
-                            "Username must be between 3 and 20 characters," +
-                                    " Password must be between 8 and 100 characters"
-                        )
-                    )
-                ), jsonPath("status").value(400)
-            ).andExpect(status().isBadRequest)
-    }
-}
-
- */
