ce091c81bcf277550a6ea3c8cff792bac3a85dbb
bohdanrysakov <bogdanrysokov@gmail.com>
2023-10-30 17:52:58 +0200

    Added second Endpoint that deliver Flux

---
+++ gRPC/src/main/kotlin/rys/ajaxpetproject/grpc/ChatFindAllService.kt
@@ -0,0 +1,46 @@
+package rys.ajaxpetproject.grpc
+
+import net.devh.boot.grpc.server.service.GrpcService
+import org.slf4j.LoggerFactory
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.toMono
+import rys.ajaxpetproject.model.MongoChat
+import rys.ajaxpetproject.request.findOne.create.proto.ChatFindOneRequest
+import rys.ajaxpetproject.request.findOne.create.proto.ChatFindOneResponse
+import rys.ajaxpetproject.request.findOne.create.proto.ReactorChatFindAllServiceGrpc
+import rys.ajaxpetproject.service.ChatService
+import rys.ajaxpetproject.utils.toProto
+
+@GrpcService
+class ChatFindAllService(private val chatService: ChatService) :
+    ReactorChatFindAllServiceGrpc.ChatFindAllServiceImplBase() {
+
+    override fun chatFindAll(request: ChatFindOneRequest?): Flux<ChatFindOneResponse> {
+        return chatService.findAll()
+            .flatMap { item ->
+                buildSuccessResponse(item.toMono())
+                    .onErrorResume { e -> Mono.just(buildFailureResponse(e)) }
+            }
+    }
+
+    private fun buildSuccessResponse(chats: Mono<MongoChat>): Mono<ChatFindOneResponse> {
+        return chats.flatMap { chat ->
+            ChatFindOneResponse.newBuilder().apply {
+                this.successBuilder.result = chat.toProto()
+            }.build().toMono()
+        }
+    }
+
+    private fun buildFailureResponse(e: Throwable): ChatFindOneResponse {
+        logger.error("Error while creating chat: ${e.message}", e)
+        return ChatFindOneResponse.newBuilder().apply {
+            failureBuilder.message = e.message
+            failureBuilder.internalErrorBuilder
+        }.build()
+    }
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(ChatFindAllService::class.java)
+    }
+}
