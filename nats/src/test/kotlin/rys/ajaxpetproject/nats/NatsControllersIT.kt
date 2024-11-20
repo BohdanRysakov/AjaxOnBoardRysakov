4a46d09daae31f708348ef2a6c85486d381fa1b8
ia14rysakov <146174709+ia14rysakov@users.noreply.github.com>
2024-11-20 11:29:30 +0200

    Update NatsControllersIT.kt

--- nats/src/test/kotlin/rys/ajaxpetproject/nats/NatsControllersIT.kt
+++ nats/src/test/kotlin/rys/ajaxpetproject/nats/NatsControllersIT.kt
@@ -12,6 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.boot.test.mock.mockito.SpyBean
 import org.springframework.test.context.ActiveProfiles
+
 import org.springframework.test.context.ContextConfiguration
 
 import reactor.kotlin.core.publisher.toFlux
